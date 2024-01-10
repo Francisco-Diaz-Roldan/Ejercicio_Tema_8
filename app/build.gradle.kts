19b781eb674e23fc7e0a054204dc18f1b39cc151
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-01-10 19:30:36 +0100

    Gradle Actualizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,9 +47,8 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.11.0")   //Para Material Design
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -60,8 +59,9 @@ dependencies {
     //Para el SplashScreen
     implementation("androidx.core:core-splashscreen:1.0.1")
     //Para GoogleMaps
-    //TODO
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     //Para Street Maps
     implementation("org.osmdroid:osmdroid-android:6.1.17")
+
 
 }
