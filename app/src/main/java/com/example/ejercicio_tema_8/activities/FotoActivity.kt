d46b6d9498f8b2c25435b8c5f8787c74fbf092c3
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-01-23 19:48:36 +0100

    Comienzo del ejercicio del tema 11: - Captura de fotos terminada(método hacerFoto en FotoActivity finalizado) Continuar con : - Guardarla fotoenla base de datos.

--- app/src/main/java/com/example/ejercicio_tema_8/activities/FotoActivity.kt
+++ app/src/main/java/com/example/ejercicio_tema_8/activities/FotoActivity.kt
@@ -1,19 +1,24 @@
 package com.example.ejercicio_tema_8.activities
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.ejercicio_tema_8.databinding.ActivityFotoBinding
+import java.util.Locale
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -21,10 +26,15 @@ class FotoActivity : AppCompatActivity() {
     private lateinit var binding:ActivityFotoBinding
     private lateinit var cameraExecutorService: ExecutorService
     private lateinit var imageCapture: ImageCapture
+    private lateinit var nombreComunidad: String
+    private var id = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFotoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        id = intent.extras!!.getInt("id")
+        nombreComunidad = intent.extras!!.getString("nombreComunidad").toString()
         //Solicitud de los permisos de la cámara
         if(allPermissionsGranted()){
             startCamera()
@@ -43,7 +53,42 @@ class FotoActivity : AppCompatActivity() {
             baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun hacerFoto(){}
+    private fun hacerFoto(){
+        //Obtenemos una referencia estable para el caso de uso de captura de imágenes
+        val imageCapture = imageCapture?: return
+        //Creamos un nombre para el archivo con la hora y dónde se va a almacenar
+        val nombreComunidad = "${nombreComunidad}_" + SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, nombreComunidad)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+        // Se establece el listener para la captura de imagen que se lanzará cuando se ha hecho una foto
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(ex: ImageCaptureException) {
+                    Log.e(TAG, "Fallo en la captura de la foto: ${ex.message}", ex)
+                }
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                val msg = "Captura de foto correcta: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        )
+    }
 
     private fun startCamera(){
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
